227a6aab5efa7ec86fe61663124b10f10eb2cc95
jasiekdrabek <33521261+jasiekdrabek@users.noreply.github.com>
2022-12-20 11:55:37 +0100

    Create Find the unique number.js

---
+++ 6 kyu/Find the unique number.js
@@ -0,0 +1,32 @@
+// There is an array with some numbers. All numbers are equal except for one. Try to find it!
+
+// findUniq([ 1, 1, 1, 2, 1, 1 ]) === 2
+// findUniq([ 0, 0, 0.55, 0, 0 ]) === 0.55
+// It’s guaranteed that array contains at least 3 numbers.
+
+// The tests contain some very huge arrays, so think about performance.
+
+// This is the first kata in series:
+
+// Find the unique number (this kata)
+// Find the unique string
+// Find The Unique
+
+function findUniq(arr) {
+    let map = new Map();
+    for (let i = 0; i < arr.length; i++) {
+        if (map.has(arr[i])) {
+            map.set(arr[i], map.get(arr[i]) + 1);
+        } else {
+            map.set(arr[i], 1);
+        }
+        if (map.length == 2 && map.get(arr[i]) >= 2) {
+            break;
+        }
+    }
+    for (let [key, value] of map) {
+        if (value == 1) {
+            return key;
+        }
+    }
+}
